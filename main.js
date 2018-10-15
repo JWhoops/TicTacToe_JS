beefcfa74c0d96fc9019adeedcbd2d65df116b0a
Jinyu Wu <q835771840@gmail.com>
2018-10-15 00:13:48 -0500

    removed user object cuz don't need it

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-//player object
-const player = (playerNum,victory) =>{
-	return{playerNum,victory}
-}
-
-//create two players for the game
-let player1, player2 
-
 //game board object
 const gameBoard = (() =>{
-	let slots = []
-	let term, win, takenSlots//count the taken slots
+	let slots = [] //slots used to store marks
+	let term, win, takenSlots //define player's term, win status, and taken slot
 	const newGame = () =>{
 			//append slots with 2
 			slots[0] = 0// ignore the base index 
@@ -17,28 +9,27 @@ const gameBoard = (() =>{
 				// use 2 to represent slots that haven't taken by player
 				slots.push(0)
 			}
-			//create two players
-		  	player1 = player(1,false)
-			player2 = player(2,false)
 			win = false //game result
 			term = 1 //player's term
 			takenSlots = 0 //set taken slots number to 0
-		}
-
+		}	
 	//markt the slot with player's number
-	const markSlot = (position,playerNum) => {
+	const markSlot = (position) => {
 		//check if slot is taken
 		if(slots[position] === 0){
-			slots[position] = playerNum //mark the slot as occupy
+			slots[position] = term //mark the slot as occupy
 			takenSlots++ //increase the taken slots number
-			term = term === 1 ? 2 : 1 //change players term
 			checkWinner(position)
+			if(win) console.log(term + " win")	
+			else term = term === 1 ? 2 : 1 //change players term
 		}
 	}
-	//check if produce victory
+	/*
+	check if produce victory
+	very dumb game logic design
+	*/
 	const checkWinner = (position) => {
-		if(takenSlots > 4 && takenSlots <= 9){
-			//game logic	
+		if(takenSlots > 4 && takenSlots <= 9){	
 			//check vertically
 			if(position <= 3){
 				if(slots[position] === slots[position + 3] && slots[position] === slots[position + 6])
@@ -52,33 +43,25 @@ const gameBoard = (() =>{
 			}
 			//check horizontally
 			switch(position) {
-			    case 1:
-			    case 4:
-			    case 7:
+			    case 1: case 4: case 7:
 			        if(slots[position] === slots[position + 1] && slots[position] === slots[position + 2])
 						win = true
 			        break;
-			    case 2:
-			    case 5:
-			    case 8:
+			    case 2: case 5: case 8:
 					if(slots[position] === slots[position - 1] && slots[position] === slots[position + 1])
 						win = true
 			        break;
-			    case 3:
-			    case 6:
-			    case 9:
+			    case 3: case 6: case 9:
 			        if(slots[position] === slots[position - 1] && slots[position] === slots[position - 2])
 						win = true
 			        break;
 			}
 			//check slantly
 			switch(position) {
-			    case 1:
-			    case 9:
+			    case 1: case 9:
 			    	if(slots[1] === slots[5] && slots[1] === slots[9]) win = true
 			        break;
-			    case 3:
-			    case 7:
+			    case 3: case 7:
 			        if(slots[3] === slots[5] && slots[3] === slots[7]) win = true
 			        break;
 			    case 5:
@@ -88,9 +71,14 @@ const gameBoard = (() =>{
 			}
 		}
 	}
-
 	const getWin = ()=>{ return win }//get win result
-	
-	return {newGame,markSlot,getWin}
+	const getTerm = ()=>{ return term }//get win result
+	return {newGame,markSlot,getWin,getTerm}
 })()
 
+gameBoard.newGame()
+gameBoard.markSlot(1)
+gameBoard.markSlot(4)
+gameBoard.markSlot(2)
+gameBoard.markSlot(5)
+gameBoard.markSlot(3)
